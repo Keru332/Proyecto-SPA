e373540f3883c7ed8a61f2dc41f92355e2000e38
Keru <kerulionhart@gmail.com>
2025-10-11 23:12:39 -0400

    Header Creado para Main View

---
+++ src/MainView/components/MainHeader.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <div class="nav-container">
+        <div class="logo">
+          Ray<span>Mei</span>Spa
+          <img src="../assets/RayMei Logo.webp" alt="logo" class="iconLogo" />
+        </div>
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/productos">Tratamientos</RouterLink>
+          <RouterLink to="/paquetes">Paquetes</RouterLink>
+          <RouterLink to="/paquetes">Login</RouterLink>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped src="./MainHeader.css"></style>
