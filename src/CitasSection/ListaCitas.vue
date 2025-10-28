a0a79b21bc30a767eb822b3aa571a7d0b892265b
Keru <kerulionhart@gmail.com>
2025-10-27 21:58:23 -0400

    d

--- src/CitasSection/ListaCitas.vue
+++ src/CitasSection/ListaCitas.vue
@@ -1,21 +1,17 @@
 <template>
   <div class="lista-cita">
-    <PaqueteBanner
-      v-for="(cita, index) in citas"
-      :key="index"
-      :cita="cita"
-    />
+    <CitasBanner v-for="(cita, index) in citas" :key="index" :cita="cita" />
   </div>
 </template>
 
 <script setup>
-import PaqueteBanner from './CitasBanner.vue'
+import CitasBanner from './CitasBanner.vue'
 
 defineProps({
   citas: {
     type: Array,
-    required: true
-  }
+    required: true,
+  },
 })
 </script>
 
