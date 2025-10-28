eaf5571d9ea669c1f5983186c2e9a015721de50f
alexanderfarinasgarcia-commits <alexander.farinas.garcia@gmail.com>
2025-10-27 20:42:20 -0400

    Update MainHeader.vue
    Quite del haeder del admin categoria no creo que haga falta

--- src/MainView/components/MainHeader.vue
+++ src/MainView/components/MainHeader.vue
@@ -41,7 +41,6 @@ const logout = () => {
           <RouterLink to="/productos">Tratamientos</RouterLink>
           <RouterLink to="/paquetes">Paquetes</RouterLink>
           <RouterLink to="/citas">Citas</RouterLink>
-          <RouterLink to="/cat">Categorías</RouterLink>
           <RouterLink to="/admin">Admin Panel</RouterLink>
           <button @click="logout" class="logout">Logout</button>
         </nav>
